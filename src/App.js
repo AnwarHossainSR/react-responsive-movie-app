8b22b63ab25a066c1ee2422decb9936785e9a4ec
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-10 21:14:33 +0600

    Setup on app.js for linking header footer and all routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "swiper/swiper.min.css";
 import "./App.scss";
-const App = () => {
-  return <div>App</div>;
-};
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Routes from "./config/Routes";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <>
+            <Header {...props} />
+            <Routes />
+            <Footer />
+          </>
+        )}
+      />
+    </BrowserRouter>
+  );
+}
 
 export default App;
